0c031b1698593bffabc980c62dcc3ad09e375bf4
Fajar Sulaksono <fajar.sulaksono@gmail.com>
2022-01-05 14:44:03 +0700

    Create model AkumulasiPoin & DataSales

---
+++ app/Models/AkumulasiPoin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AkumulasiPoin extends Model
+{
+    protected $table = 'akumulasi_poin';
+    protected $guarded = [];
+}
---
+++ app/Models/DataSales.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DataSales extends Model
+{
+    protected $table = 'data_sales';
+    protected $guarded = [];
+}
