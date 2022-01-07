e7c38d5bcdf8dbb2263df18db7a833d69293e42f
Fajar Sulaksono <fajar.sulaksono@gmail.com>
2022-01-07 16:11:18 +0700

    *

--- app/Console/Commands/CekPoinMembership.php
+++ app/Console/Commands/CekPoinMembership.php
@@ -3,7 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-
+use App\Models\AkumulasiPoin;
+use App\Models\UnicharmMember;
 class CekPoinMembership extends Command
 {
     /**
@@ -11,7 +12,7 @@ class CekPoinMembership extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'cek:poin_member_status';
 
     /**
      * The console command description.
@@ -37,6 +38,22 @@ class CekPoinMembership extends Command
      */
     public function handle()
     {
-        return 0;
+        $data_poin = AkumulasiPoin::whereNull('status_cek_membership')->take(80000)->get();
+
+        foreach ($data_poin as $dp) {
+            $member = UnicharmMember::where('no_hp', trim($dp->no_hp))->first();
+
+            if ($member) {
+                echo 'member-found : '.$dp->id. "\n";
+
+                $data_poin = AkumulasiPoin::find($dp->id);
+                $data_poin->id_member = $member->id_member;
+                $data_poin->status_cek_membership = '1';
+                $data_poin->save();
+            } else {
+                echo 'member-not-found : '.$dp->id. "\n";
+            }
+        }
+        //return 0;
     }
 }
