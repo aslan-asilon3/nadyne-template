4ed770b02396f2c5be83ea62fd9c072ae162f441
Fajar Sulaksono <fajar.sulaksono@gmail.com>
2022-01-11 16:43:23 +0700

    *

--- resources/views/akumulasi_poin_index.blade.php
+++ resources/views/akumulasi_poin_index.blade.php
@@ -15,7 +15,7 @@
             <div class="col-12">
                 <button type="button" class="btn btn-primary btn-flat" id="filter-show"><i class="fas fa-search"></i> Filter</button>
                 <button type="button" class="btn btn-secondary btn-reset btn-flat" id="reset" style="display:none"><i class="fas fa-sync"></i> Reset</button>
-                <button type="button" id="download" class="btn btn-success btn-flat"><i class="fas fa-download"></i> Download Excel</button>
+                <button type="button" id="export_excel" class="btn btn-success btn-flat"><i class="fas fa-download"></i> Download Excel</button>
             </div>
         </div>
         <div class="mb-3"></div>
