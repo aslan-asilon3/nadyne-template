904401b8ca8a049b3abfe9f7b892b3f741a49c00
Fajar Sulaksono <fajar.sulaksono@gmail.com>
2022-01-12 13:20:51 +0700

    *

--- resources/views/data_sales_index.blade.php
+++ resources/views/data_sales_index.blade.php
@@ -76,7 +76,7 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button id="submit" class="btn btn-primary btn-flat">Submit</button>
+                    <button id="submit-filter" class="btn btn-primary btn-flat">Submit</button>
                 </div>
             </div>
         </form>
