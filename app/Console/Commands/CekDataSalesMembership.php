2546ac160a5292273390472c2672cf140fc6af2f
Fajar Sulaksono <fajar.sulaksono@gmail.com>
2022-02-08 15:41:36 +0700

    Cek membership data sales 2022-03

--- app/Console/Commands/CekDataSalesMembership.php
+++ app/Console/Commands/CekDataSalesMembership.php
@@ -42,7 +42,7 @@ class CekDataSalesMembership extends Command
         $data_sales = DataSales::whereNull('status_cek_is_member')
                 //'2021-07','2021-08','2021-09'
                 //
-                ->whereIn('batch', ['2021-23','2021-49'])
+                ->whereIn('batch', ['2022-03'])
                 ->take(10000)
                 ->get();
 
