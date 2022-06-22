00a569a8e32caeb6caa5fa920c4c4f7c05280643
Sulaslan Setiawan <aslan@nadyne.com>
2022-06-22 13:44:21 +0700

    insert into data member raw update data member

--- app/Imports/ImportUnicharmMemberRaw.php
+++ app/Imports/ImportUnicharmMemberRaw.php
@@ -23,17 +23,10 @@ class ImportUnicharmMemberRaw implements ToModel, WithStartRow
         $UpdateMember->save();
 
 
-        // dd($cekUpdate);
-
-
-
-        // ========logic 3
         $cekNohp = UnicharmMember::where('no_hp', '=', $this->cek($row[2]))->first();
         // dd($cekNohp);
 
         if($cekNohp){
-            // $data = UnicharmMember::where('id', '=', $cekNohp->id)->first();
-            // dd($data);
             
             $cekNohp->update([
                 'id_member' => $this->cek($row[1])
@@ -46,51 +39,7 @@ class ImportUnicharmMemberRaw implements ToModel, WithStartRow
             'no_hp'     => $this->cek($row[2])
         ]);
         }
-        // ========End logic 3
-
-        // // ========logic 2
-        // if($cekUpdate){
-        //     UnicharmMember::create([
-        //     $cekUpdate->id_member= $row[1],
-        //     $cekUpdate->save()
-        //     ]);
-
-        // }else{
-        //     return new UnicharmMember([
-        //     $cekUpdate->id_member= $row[1],
-        //     $cekUpdate->save()
-        //   ]);
-        // }
-        // // ========End logic 2
-
-
-        // // ======== ke import logic 1
-        // $cekUpdate = UnicharmMember::where("no_hp", $this->cek($row[2]))->first();
-        // // apabila sudah ada no-hp nya, 
-        // // diupdate id-membernya dengan id-member yang terbaru
-        // if($cekUpdate){
-        //     $cekUpdate->id_member= $row[1];
-        //     $cekUpdate->save();
-        // }else{
-            
-        //     // apabila blm ada no-hp nya, 
-        //      // diinsert ke unicharm_member no-hp & id_member-nya
-        //     return new UnicharmMember([
-        //     // 'id' => $row[0],
-        //     'id_member' => $row[1],
-        //     'no_hp' => $this->cek($row[2]),
-        // ]);
-        // }
-
-        // // ========End logic 1
-
-        
-        // return new UnicharmMemberRaw([
-        //     // 'id' => $row[0],
-        //     'id_member' => $row[1],
-        //     'no_hp' => $this->cek($row[2]),
-        //     'status_cek_data' => $row[3],
-        // ]);
+ 
     }
 
     public function startRow(): int
