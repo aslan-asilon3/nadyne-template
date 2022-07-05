cf0efdcc5ffebb0ef424465fb9ebad245e68eeb5
fajar <fajar.sulaksono@gmail.com>
2022-07-05 15:01:39 +0700

    *

--- app/Console/Commands/CleanMemberRaw.php
+++ app/Console/Commands/CleanMemberRaw.php
@@ -41,7 +41,7 @@ class CleanMemberRaw extends Command
     {
         $member_raw = UnicharmMemberRaw::whereNull('status_cek_data')
             //1611150
-            ->where('id', '>' ,1697647)
+            ->where('id', '>' ,1707385)
             ->take(60000)
             ->get();
 
